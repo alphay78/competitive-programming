c350a2aede8598c345b4c3ebed07a0d9b31af0ce
Alpha_Degago <148535861+alphay78@users.noreply.github.com>
2024-06-24 20:58:29 +0300

    Time: 454 ms (96.07%) | Memory: 22.3 MB (70.58%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
-        index_map = {}
+        index_map = {}  # Dictionary to store the last seen index of each element
+
         for i, num in enumerate(nums):
-           
+            # Check if num is in the dictionary and if the difference in indices is <= k
             if num in index_map and i - index_map[num] <= k:
-                return True
+                return True  # Duplicate found within the range k
             
+            # Update the last seen index of the current number
             index_map[num] = i
-        
-        return False
+
+        return False  # No duplicates found within the range k
