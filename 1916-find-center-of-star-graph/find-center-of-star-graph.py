b1ffd90ddd4435cd9236f7397aebe7e8fc22db45
Alpha_Degago <148535861+alphay78@users.noreply.github.com>
2024-08-29 08:22:33 +0300

    Time: 625 ms (99.13%) | Memory: 52.1 MB (36.75%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,7 +1,5 @@
 class Solution:
     def findCenter(self, edges: list[list[int]]) -> int:
-        # The center of the star graph will be either one of the nodes from the first edge
-        # Check which one of them is also present in the second edge
         if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
             return edges[0][0]
         else:
