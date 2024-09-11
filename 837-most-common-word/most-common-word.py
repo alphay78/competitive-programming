c12af1e471f557eede3f30eb23b57b2ceddee24c
Alpha_Degago <148535861+alphay78@users.noreply.github.com>
2024-09-11 08:28:23 +0300

    Time: 43 ms (30.87%) | Memory: 16.7 MB (25.06%) - LeetSync

--- 837-most-common-word/most-common-word.py
+++ 837-most-common-word/most-common-word.py
@@ -1,22 +1,18 @@
 import re
+from collections import Counter
 
 class Solution(object):
     def mostCommonWord(self, paragraph, banned):
         lowerCase_paragraph = paragraph.lower()
         clean_paragraph = re.sub(r'[^\w\s]', ' ', lowerCase_paragraph)
-       
+        
         words = clean_paragraph.split()
         
-        word_count = {}
+        banned_set = set(banned)
         
-        banned_set = set(banned)  
-        for word in words:
-            if word not in banned_set:
-                if word in word_count:
-                    word_count[word] += 1
-                else:
-                    word_count[word] = 1
+        word_count = Counter(word for word in words if word not in banned_set)
         
-        most_common_word = max(word_count, key=word_count.get)
-        
+        # Find the most common word
+        most_common_word = max( word_count, key= word_count.get)
+
         return most_common_word
