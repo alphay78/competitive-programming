b800c2b9aab5577f15016c6e09bd436b9cfc2303
Alpha_Degago <148535861+alphay78@users.noreply.github.com>
2024-10-03 07:19:05 +0300

    Time: 57 ms (17.67%) | Memory: 17.4 MB (19.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,25 +1,25 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+#         count_s = {}
+#         count_t = {}
+        
+#         for char in s:
+#             if char in count_s:
+#                 count_s[char] += 1
+#             else:
+#                 count_s[char] = 1
+                
+#         for char in t:
+#             if char in count_t:
+#                 count_t[char] += 1
+#             else:
+#                 count_t[char] = 1
+        
+       
+#         return count_s == count_t
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        # Count characters in both strings
-        count_s = {}
-        count_t = {}
-        
-        for char in s:
-            if char in count_s:
-                count_s[char] += 1
-            else:
-                count_s[char] = 1
-                
-        for char in t:
-            if char in count_t:
-                count_t[char] += 1
-            else:
-                count_t[char] = 1
-        
-        # Compare character counts
-        return count_s == count_t
-
-
+        return sorted(s)==sorted(t)
