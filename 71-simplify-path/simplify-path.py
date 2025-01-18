c6edaf233325f27eb2d094b8a1230b4c5ae9a836
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-01-18 07:10:35 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.44%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -18,11 +18,3 @@ class Solution:
         simplified_path = "/" + "/".join(stack)
         
         return simplified_path
-
-# Example usage:
-sol = Solution()
-print(sol.simplifyPath("/home/"))  # Output: "/home"
-print(sol.simplifyPath("/home//foo/"))  # Output: "/home/foo"
-print(sol.simplifyPath("/home/user/Documents/../Pictures"))  # Output: "/home/user/Pictures"
-print(sol.simplifyPath("/../"))  # Output: "/"
-print(sol.simplifyPath("/.../a/../b/c/../d/./"))  # Output: "/.../b/d"
