e5a2b1c563c858974e6dd91f5b999d5b3a894fcd
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-01-18 07:17:26 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (50.75%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,20 +1,36 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        # stack = []
+        
+        # # Split the path by "/" to get individual components
+        # components = path.split('/')
+        
+        # for component in components:
+        #     if component == "..":
+        #         if stack:
+        #             stack.pop()
+        #     elif component == "." or component == "":
+        #         continue
+        #     else:
+        #         stack.append(component)
+        
+        # # Join the stack with "/" to form the canonical path
+        # simplified_path = "/" + "/".join(stack)
+        
+        # return simplified_path
+
         stack = []
-        
-        # Split the path by "/" to get individual components
-        components = path.split('/')
-        
-        for component in components:
-            if component == "..":
-                if stack:
-                    stack.pop()
-            elif component == "." or component == "":
-                continue
+        cur = ""
+
+        for i in path + "/":
+            if i == "/":
+                if cur == "..":
+                    if stack : stack.pop()
+                elif cur != "" and cur != ".":
+                    stack.append(cur)
+                cur = ""
+
             else:
-                stack.append(component)
-        
-        # Join the stack with "/" to form the canonical path
-        simplified_path = "/" + "/".join(stack)
-        
-        return simplified_path
+                cur += i
+        return "/" + "/".join(stack)
+
