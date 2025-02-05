a0088c07ba28bf51a2d755386d64e3ce830c49fc
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-02-05 09:02:46 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (28.17%) - LeetSync

---
+++ 722-remove-comments/remove-comments.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        in_comment = False
+        ans = []
+        for line in source:
+            i = 0
+            if not in_comment:
+                newline = []
+            while i < len(line):
+                if line[i:i+2] == '/*' and not in_comment:
+                    in_comment = True
+                    i += 1
+                elif line[i:i+2] == "*/" and in_comment:
+                    in_comment = False
+                    i += 1
+                elif not in_comment and line[i:i+2] == '//':
+                    break
+                elif not in_comment:
+                    newline.append(line[i])
+                i+=1
+
+            if newline and not in_comment:
+                ans.append("".join(newline))
+        return ans
+
+                
+
+
+        
