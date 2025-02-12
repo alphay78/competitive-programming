29cb2f6526342d72685870289aa2bbb4fa197f79
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-02-12 16:16:01 +0300

    Time: 541 ms (19.77%) | Memory: 33.3 MB (87.02%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,15 +1,12 @@
-from typing import List
-from collections import defaultdict
-
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        digit_sum_map = defaultdict(list)  # Dictionary to store numbers by digit sum
-        max_sum = -1  # Default if no valid pair exists
+        digit_sum_map = defaultdict(list)  
+        max_sum = -1 
         
         # Function to calculate the sum of digits of a number
         def digitSum(num):
             return sum(int(digit) for digit in str(num))
-        
+    
         # Group numbers by their digit sum
         for num in nums:
             key = digitSum(num)
