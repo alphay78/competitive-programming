56f327737a8e64ff9b330c7cd7dc52f58df169d8
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-02-14 00:19:41 +0300

    Time: 370 ms (32.92%) | Memory: 17.7 MB (75.55%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -5,20 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+
         nums.sort()
         closest_sum = float('inf')
-        
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
+
+        for i in range(len(nums)-2):
+            left , right = i+1 , len(nums)-1
+
             while left < right:
                 current_sum = nums[i] + nums[left] + nums[right]
                 if abs(current_sum - target) < abs(closest_sum - target):
                     closest_sum = current_sum
                 if current_sum < target:
-                    left += 1
+                    left+=1
                 elif current_sum > target:
-                    right -= 1
+                    right-=1
                 else:
                     return current_sum
-        
         return closest_sum
+
