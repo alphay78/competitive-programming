5d4430fca6c8e25893c331b5055f0689e51dd237
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-02-19 15:59:20 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.58%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -3,7 +3,7 @@ from typing import Optional
 class ListNode:
     def __init__(self, x):
         self.val = x
-        self.next = None  # Fix the next pointer initialization
+        self.next = None 
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
