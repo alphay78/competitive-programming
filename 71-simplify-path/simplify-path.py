07f9553581e45ef23ad0797df96f58ae5b89333a
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-03-13 12:18:50 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (76.91%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,36 +1,13 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # stack = []
-        
-        # # Split the path by "/" to get individual components
-        # components = path.split('/')
-        
-        # for component in components:
-        #     if component == "..":
-        #         if stack:
-        #             stack.pop()
-        #     elif component == "." or component == "":
-        #         continue
-        #     else:
-        #         stack.append(component)
-        
-        # # Join the stack with "/" to form the canonical path
-        # simplified_path = "/" + "/".join(stack)
-        
-        # return simplified_path
-
+        path = path.split('/')  
         stack = []
-        cur = ""
-
-        for i in path + "/":
-            if i == "/":
-                if cur == "..":
-                    if stack : stack.pop()
-                elif cur != "" and cur != ".":
-                    stack.append(cur)
-                cur = ""
-
-            else:
-                cur += i
-        return "/" + "/".join(stack)
-
+        
+        for dir in path:
+            if dir == "..":  
+                if stack:
+                    stack.pop()
+            elif dir != "." and dir != "":  
+                stack.append(dir)
+        
+        return "/" + "/".join(stack)  
