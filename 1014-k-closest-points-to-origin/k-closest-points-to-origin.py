044ab4e2de0ea70c6493e16a977882b9507493be
Alpha Degago <148535861+alphay78@users.noreply.github.com>
2025-08-01 22:09:19 +0300

    Time: 59 ms (77.80%) | Memory: 22.9 MB (34.60%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,16 +1,11 @@
-import heapq
-from typing import List
-
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         max_heap = []
 
         for x, y in points:
-            dist = -(x * x + y * y)  # Negative distance to simulate max-heap
-            heapq.heappush(max_heap, (dist, x, y))  # Push into heap
-
+            dist = -(x * x + y * y) 
+            heapq.heappush(max_heap, (dist, x, y))  
             if len(max_heap) > k:
-                heapq.heappop(max_heap)  # Remove the farthest point
+                heapq.heappop(max_heap)
 
-        # Extract only the coordinates from the heap
         return [[x, y] for (_, x, y) in max_heap]
